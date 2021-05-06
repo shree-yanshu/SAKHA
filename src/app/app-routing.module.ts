009b2d15c70b82d30dc32064f9a9a66de310881d
uttamv <49646294+uttamv@users.noreply.github.com>
2021-05-06 19:59:59 +0530

    completed footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
-
+//import { FooterComponent } from './footer/footer.component';
 const routes: Routes = [
   {path:'', component:HomeComponent}
+  //{path:'footer', component:FooterComponent},
 ];
 
 @NgModule({
