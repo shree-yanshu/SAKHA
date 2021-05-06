d3e9ee3067b0df8214c0f6f4667eced45f4c161e
Raghav <raghavgoel2701@gmail.com>
2021-05-06 23:54:48 +0530

    Linked contact us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { RouterModule, Routes } from '@angular/router';
-//import { FooterComponent } from './footer/footer.component';
+
 const routes: Routes = [
-  {path:'', component:HomeComponent}
-  //{path:'footer', component:FooterComponent},
   {path:'', component:HomeComponent},
   {path:'contact-us', component:ContactUsComponent}
 ];
