96f6ee2ee12877ced2fbf6bf9849e2df411a5b87
Raghav <raghavgoel2701@gmail.com>
2021-05-06 23:36:47 +0530

    Added Contact Us Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent}
+  {path:'', component:HomeComponent},
+  {path:'contact-us', component:ContactUsComponent}
 ];
 
 @NgModule({
